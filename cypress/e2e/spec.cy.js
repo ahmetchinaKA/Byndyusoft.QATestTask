0e41d2f8a92ab809fa58fa03a06073de43535ac9
ahmetchinaKA <ahmetchina.ka@gmail.com>
2022-12-07 20:46:51 +0500

    tests for contact information were implemented

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -14,4 +14,16 @@ describe("Проверка контактной информации", () => {
   })
 })
 })
-
+it("Ожидаемый почтовый ящик: sales@byndyusoft.com", () => {
+  cy.visit('https://www.google.ru/')
+  cy.get('input[name="q"]').type('Byndyusoft{enter}')
+  cy.get('[id=search]').within(() => {
+    cy.get('a').first().invoke('attr', 'href').then((url) => {
+      cy.origin(url, () => {
+        cy.visit('/')
+        cy.get('.know-more')
+        cy.contains('Заказать презентацию').click()
+        cy.get('.popup-callback__content').should('contain.text', 'sales@byndyusoft.com')
+    })
+  })})
+});
